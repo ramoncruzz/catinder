3c66f00f43aa9d3c70d267e975c337f8c6a39064
ABNER BORDA FONSECA <abner.fonseca@C02CR37QMD6P.local>
2022-07-11 09:14:53 -0300

    adicionando ci/cd no circle-ci

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import { SafeAreaView, View} from 'react-native';
-import {StackCardSwiper} from '../../components';
-import {trackComponent, trackPageView } from '../../utils/trackTestID';
-import {useTheCat} from '../../hooks'
-import Styles from './styles';
+import React from "react";
+import { SafeAreaView, View } from "react-native";
+import { StackCardSwiper } from "../../components";
+import { trackComponent, trackPageView } from "../../utils/trackTestID";
+import { useTheCat } from "../../hooks";
+import Styles from "./styles";
 
-const namePageForTesting= "home";
-const Home: React.FC = ():JSX.Element =>{
-    const {catList, setPage} = useTheCat();
-    return(
+const namePageForTesting = "home";
+const Home: React.FC = (): JSX.Element => {
+  const { catList, setPage } = useTheCat();
+  console.log("para deletar");
+
+  return (
     <SafeAreaView style={Styles.page}>
-        <View testID={trackPageView(namePageForTesting,'page')} style={Styles.container}>
-          <StackCardSwiper  onMatch={(index)=>{console.log(`match ${index}`)}} onNotMatch={(index)=>{console.log(`note match ${index}`)}} cats={catList} testID={trackComponent(namePageForTesting,'StackCardSwiper')}/>
-        </View>
+      <View
+        testID={trackPageView(namePageForTesting, "page")}
+        style={Styles.container}
+      >
+        <StackCardSwiper
+          onMatch={index => {
+            console.log(`match ${index}`);
+          }}
+          onNotMatch={index => {
+            console.log(`note match ${index}`);
+          }}
+          cats={catList}
+          testID={trackComponent(namePageForTesting, "StackCardSwiper")}
+        />
+      </View>
     </SafeAreaView>
-)}
+  );
+};
 
 export default Home;
